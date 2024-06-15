74f04cb3a751cda60a39093f5b0e82da85d665d1
Nguyen Huu Hieu <hieunguyen12920012001@gmail.com>
2024-06-15 22:18:06 +0700

    frame

---
+++ Assets/Resources/Scripts/Background/Frame2.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Frame2 : MonoBehaviour
+{
+	[SerializeField]
+	GameObject parentObject;
+	Sprite playPrefap;
+	GameObject childGameObject;
+
+	string extend, shipFight, tmpShip;
+	int check;
+	void Start()
+	{
+		PlayerPrefs.SetString("tmpShip", "default");
+		UpdateShip();
+	}
+	void Update()
+	{
+		check = PlayerPrefs.GetInt("check");
+		if (check == 1) UpdateShip();
+	}
+	void UpdateShip()
+	{
+		shipFight = PlayerPrefs.GetString("ShipFight", "ship1");
+		tmpShip = PlayerPrefs.GetString("tmpShip");
+
+		if (tmpShip.Equals("ship3"))
+		{
+			extend = ".asset";
+		}
+		else
+		{
+			extend = ".png";
+		}
+		if (tmpShip.Equals("default"))
+		{
+			if (shipFight.Equals("ship3"))
+			{
+				playPrefap = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Resources/Sprites/Ships/Player/" + shipFight + ".asset", typeof(Sprite));
+			}
+			else
+			{
+				playPrefap = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Resources/Sprites/Ships/Player/" + shipFight + extend, typeof(Sprite));
+			}
+		}
+		else
+		{
+			playPrefap = (Sprite)AssetDatabase.LoadAssetAtPath("Assets/Resources/Sprites/Ships/Player/" + tmpShip + extend, typeof(Sprite));
+		}
+		CreateChildObj();
+	}
+	void CreateChildObj()
+	{
+		if (parentObject != null && parentObject.transform.childCount <= 0)
+		{
+			childGameObject = new GameObject("Playerimage");
+			childGameObject.transform.parent = parentObject.transform;
+			SpriteRenderer childSpriteRenderer = childGameObject.AddComponent<SpriteRenderer>();
+
+			childSpriteRenderer.sprite = playPrefap;
+			childSpriteRenderer.sortingOrder = 1;
+			childGameObject.transform.localPosition = new Vector3(0.01f, 0f, 0f);
+
+			childGameObject.transform.localRotation = Quaternion.identity;
+			childGameObject.transform.localScale = new Vector3(0.09f, 0.09f, 0f);
+		}
+		else
+		{
+			SpriteRenderer spriteRenderer = childGameObject.GetComponent<SpriteRenderer>();
+			spriteRenderer.sprite = playPrefap;
+			spriteRenderer.sortingOrder = 1;
+
+			childGameObject.transform.localPosition = new Vector3(0.01f, 0f, 0f);
+			childGameObject.transform.localRotation = Quaternion.identity;
+			childGameObject.transform.localScale = new Vector3(0.09f, 0.09f, 0f);
+		}
+		
+	}
+}
--- Assets/Resources/Scripts/Button/OnOffPanel.cs
+++ Assets/Resources/Scripts/Button/OnOffPanel.cs
@@ -1,32 +1,131 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class OnOffPanel : MonoBehaviour
 {
 	bool onOff = false;
+	bool onOffShip = false;
+	bool onOffBullet = false;
+	bool onOffChild = false;
 	[SerializeField]
-	 GameObject popupPanel;
+	GameObject popupPanel;
 	public void btnOnOffPanel(string shipName)
 	{
 		Active(shipName);
 	}
+
 	void Active(string shipName)
 	{
-		PlayerPrefs.SetInt("check", 1);
-		PlayerPrefs.SetString("tmpShip", shipName);
+		
 		if (onOff != true)
 		{
 			onOff = true;
 			popupPanel.SetActive(true);
 		}
+		else if (shipName.Equals("close"))
+		{
+			onOff = false;
+			popupPanel.SetActive(false);
+		}
 		else
 		{
+			PlayerPrefs.SetInt("check", 1);
+			PlayerPrefs.SetString("tmpShip", shipName);
 			PlayerPrefs.SetString("ShipName", shipName);
 			PlayerPrefs.Save();
 			onOff = false;
 			popupPanel.SetActive(false);
 		}
-		
+
 	}
+	//===========================================
+	public void btnOnOffPanelChild(string child)
+	{
+		ActiveChild(child);
+	}
+	void ActiveChild(string child)
+	{
+
+		if (onOff != true && child.Equals("null"))
+		{
+			onOff = true;
+			popupPanel.SetActive(true);
+		}
+		else if (child.Equals("close"))
+		{
+			onOff = false;
+			popupPanel.SetActive(false);
+		}
+		else
+		{
+			PlayerPrefs.SetInt("check", 1);
+			PlayerPrefs.SetString("tmpChild", child);
+			PlayerPrefs.SetString("ChildName", child);
+			PlayerPrefs.Save();
+			onOff = false;
+			popupPanel.SetActive(false);
+		}
+
+	}
+	//===========================================
+	public void btnOnOffPanelShip(string shipName)
+	{
+		ActiveShip(shipName);
+	}
+	void ActiveShip(string shipName)
+	{
+
+		if (onOff != true && shipName.Equals("null"))
+		{
+			onOff = true;
+			popupPanel.SetActive(true);
+		}
+		else
+		if (shipName.Equals("close"))
+		{
+			onOff = false;
+			popupPanel.SetActive(false);
+		}
+		else
+		{
+			PlayerPrefs.SetInt("check", 1);
+			PlayerPrefs.SetString("Ship1", shipName);
+			PlayerPrefs.SetString("ShipName1", shipName);
+			PlayerPrefs.Save();
+			onOff = false;
+			popupPanel.SetActive(false);
+		}
+
+	}
+	//=================================
+	public void btnOnOffPanelBullet(string bullet)
+	{
+		ActiveBullet(bullet);
+	}
+	void ActiveBullet(string bullet)
+	{
+		if (onOff != true && bullet.Equals("null"))
+		{
+			onOff = true;
+			popupPanel.SetActive(true);
+		}
+		else if (bullet.Equals("close"))
+		{
+			onOff = false;
+			popupPanel.SetActive(false);
+		}
+		else
+		{
+			PlayerPrefs.SetInt("check", 1);
+			PlayerPrefs.SetString("tmpBullet", bullet);
+			PlayerPrefs.SetString("BulletName", bullet);
+			PlayerPrefs.Save();
+			onOff = false;
+			popupPanel.SetActive(false);
+		}
+
+	}
+
 }
